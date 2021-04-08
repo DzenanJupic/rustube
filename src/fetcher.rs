cdd47408c654061763a7ecca17402823ef80f2ed
Dzenan Jupic <56133904+DzenanJupic@users.noreply.github.com>
2021-04-08 11:23:43 +0200

    Fixed a problem regarding YouTube's new consent mechanism
    YouTube recently changed the way some users have to agree to cookies, so that some users (probably only european users) are redirected to a dedicated site, that asks for cookie approval. That lead to an error, since the watch html didn't contain the correct information anymore.
    This error can be fixed by supplying some headers and cookies.
    
    Closes #8

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -78,8 +78,8 @@ impl VideoFetcher {
     /// - When [`Id::from_raw`] fails to extracted the videos id from the url.
     /// - When [`reqwest`] fails to initialize an new [`Client`].
     #[inline]
-    #[cfg(any(feature = "regex", doc))]
     #[doc(cfg(feature = "regex"))]
+    #[cfg(any(feature = "regex", doc))]
     pub fn from_url(url: &Url) -> crate::Result<Self> {
         let id = Id::from_raw(url.as_str())?
             .into_owned();
@@ -91,14 +91,23 @@ impl VideoFetcher {
     /// When [`reqwest`] fails to initialize an new [`Client`].
     #[inline]
     pub fn from_id(video_id: IdBuf) -> crate::Result<Self> {
+        // maybe make these feature gated, to prevent overhead for users that
+        //  don't have problems with youtube consent
+        let cookie_jar = recommended_cookies();
+        let headers = recommended_headers();
+
         let client = Client::builder()
-            .cookie_store(true)
+            .default_headers(headers)
+            .cookie_provider(std::sync::Arc::new(cookie_jar))
             .build()?;
+
         Ok(Self::from_id_with_client(video_id, client))
     }
 
     /// Constructs a [`VideoFetcher`] from an [`Id`] and an existing [`Client`].
     /// There are no special constrains, what the [`Client`] has to look like.
+    /// It's recommended to use the cookie jar returned from [`recommended_cookies`]. 
+    /// It's recommended to use the headers returned from [`recommended_headers`]. 
     #[inline]
     pub fn from_id_with_client(video_id: IdBuf, client: Client) -> Self {
         Self {
@@ -118,8 +127,8 @@ impl VideoFetcher {
     /// When having a good internet connection, only errors due to inaccessible videos should occur.
     /// Other errors usually mean, that YouTube changed their API, and `rustube` did not adapt to 
     /// this change yet. Please feel free to open a GitHub issue if this is the case.
-    #[cfg(any(feature = "fetch", doc))]
     #[doc(cfg(feature = "fetch"))]
+    #[cfg(any(feature = "fetch", doc))]
     pub async fn fetch(self) -> crate::Result<VideoDescrambler> {
         // fixme: 
         //  It seems like watch_html also contains a PlayerResponse in all cases. VideoInfo
@@ -205,8 +214,8 @@ impl VideoFetcher {
 
     /// Extracts or requests the JavaScript used to descramble the video signature.
     #[inline]
-    #[cfg(any(feature = "fetch", doc))]
     #[doc(cfg(feature = "fetch"))]
+    #[cfg(any(feature = "fetch", doc))]
     async fn get_js(
         &self,
         is_age_restricted: bool,
@@ -229,8 +238,8 @@ impl VideoFetcher {
 
     /// Requests the [`VideoInfo`] of a video 
     #[inline]
-    #[cfg(any(feature = "fetch", doc))]
     #[doc(cfg(feature = "fetch"))]
+    #[cfg(any(feature = "fetch", doc))]
     async fn get_video_info(&self, is_age_restricted: bool) -> crate::Result<VideoInfo> {
         let video_info_url = self.get_video_info_url(is_age_restricted);
         let video_info_raw = self.get_html(&video_info_url).await?;
@@ -243,8 +252,8 @@ impl VideoFetcher {
 
     /// Generates the url under which the [`VideoInfo`] can be requested.
     #[inline]
-    #[cfg(any(feature = "fetch", doc))]
     #[doc(cfg(feature = "fetch"))]
+    #[cfg(any(feature = "fetch", doc))]
     fn get_video_info_url(&self, is_age_restricted: bool) -> Url {
         if is_age_restricted {
             video_info_url_age_restricted(
@@ -261,8 +270,8 @@ impl VideoFetcher {
 
     /// Requests a website.
     #[inline]
-    #[cfg(any(feature = "fetch", doc))]
     #[doc(cfg(feature = "fetch"))]
+    #[cfg(any(feature = "fetch", doc))]
     async fn get_html(&self, url: &Url) -> crate::Result<String> {
         Ok(
             self.client
@@ -277,8 +286,8 @@ impl VideoFetcher {
 
 /// Extracts whether or not a particular video is age restricted. 
 #[inline]
-#[cfg(any(feature = "fetch", doc))]
 #[doc(cfg(feature = "fetch"))]
+#[cfg(any(feature = "fetch", doc))]
 fn is_age_restricted(watch_html: &str) -> bool {
     static PATTERN: SyncLazy<Regex> = SyncLazy::new(|| Regex::new("og:restrictions:age").unwrap());
     PATTERN.is_match(watch_html)
@@ -286,8 +295,8 @@ fn is_age_restricted(watch_html: &str) -> bool {
 
 /// Generates the url under which the [`VideoInfo`] of a video can be requested. 
 #[inline]
-#[cfg(any(feature = "fetch", doc))]
 #[doc(cfg(feature = "fetch"))]
+#[cfg(any(feature = "fetch", doc))]
 fn video_info_url(video_id: Id<'_>, watch_url: &Url) -> Url {
     let params: &[(&str, &str)] = &[
         ("video_id", video_id.as_str()),
@@ -300,8 +309,8 @@ fn video_info_url(video_id: Id<'_>, watch_url: &Url) -> Url {
 
 /// Generates the url under which the [`VideoInfo`] of an age restricted video can be requested.
 #[inline]
-#[cfg(any(feature = "fetch", doc))]
 #[doc(cfg(feature = "fetch"))]
+#[cfg(any(feature = "fetch", doc))]
 fn video_info_url_age_restricted(video_id: Id<'_>, watch_url: &Url) -> Url {
     static PATTERN: SyncLazy<Regex> = SyncLazy::new(|| Regex::new(r#""sts"\s*:\s*(\d+)"#).unwrap());
 
@@ -321,8 +330,8 @@ fn video_info_url_age_restricted(video_id: Id<'_>, watch_url: &Url) -> Url {
 
 /// Helper for assembling th video info url.
 #[inline]
-#[cfg(any(feature = "fetch", doc))]
 #[doc(cfg(feature = "fetch"))]
+#[cfg(any(feature = "fetch", doc))]
 fn _video_info_url(params: &[(&str, &str)]) -> Url {
     Url::parse_with_params(
         "https://youtube.com/get_video_info?",
@@ -332,8 +341,8 @@ fn _video_info_url(params: &[(&str, &str)]) -> Url {
 
 /// Generates the url under which the JavaScript used for descrambling can be requested.
 #[inline]
-#[cfg(any(feature = "fetch", doc))]
 #[doc(cfg(feature = "fetch"))]
+#[cfg(any(feature = "fetch", doc))]
 fn js_url(html: &str) -> crate::Result<(Url, Option<PlayerResponse>)> {
     let player_response = get_ytplayer_config(html);
     let base_js = match player_response {
@@ -346,8 +355,8 @@ fn js_url(html: &str) -> crate::Result<(Url, Option<PlayerResponse>)> {
 
 /// Extracts the [`PlayerResponse`] from the watch html.
 #[inline]
-#[cfg(any(feature = "fetch", doc))]
 #[doc(cfg(feature = "fetch"))]
+#[cfg(any(feature = "fetch", doc))]
 fn get_ytplayer_config(html: &str) -> crate::Result<PlayerResponse> {
     static CONFIG_PATTERNS: SyncLazy<[Regex; 3]> = SyncLazy::new(|| [
         Regex::new(r"ytplayer\.config\s*=\s*").unwrap(),
@@ -378,8 +387,8 @@ fn get_ytplayer_config(html: &str) -> crate::Result<PlayerResponse> {
 
 /// Extracts a json object from a string starting after a pattern.
 #[inline]
-#[cfg(any(feature = "fetch", doc))]
 #[doc(cfg(feature = "fetch"))]
+#[cfg(any(feature = "fetch", doc))]
 fn parse_for_object<'a>(html: &'a str, regex: &Regex) -> crate::Result<&'a str> {
     let json_obj_start = regex
         .find(html)
@@ -395,8 +404,8 @@ fn parse_for_object<'a>(html: &'a str, regex: &Regex) -> crate::Result<&'a str> 
 
 /// Deserializes the [`PalyerResponse`] which can be found in the watch html.
 #[inline]
-#[cfg(any(feature = "fetch", doc))]
 #[doc(cfg(feature = "fetch"))]
+#[cfg(any(feature = "fetch", doc))]
 fn deserialize_ytplayer_config(json: &str) -> crate::Result<PlayerResponse> {
     #[derive(Deserialize)]
     struct Args { player_response: PlayerResponse }
@@ -414,8 +423,8 @@ fn deserialize_ytplayer_config(json: &str) -> crate::Result<PlayerResponse> {
 
 /// Extracts the JavaScript used for descrambling from the watch html.
 #[inline]
-#[cfg(any(feature = "fetch", doc))]
 #[doc(cfg(feature = "fetch"))]
+#[cfg(any(feature = "fetch", doc))]
 fn get_ytplayer_js(html: &str) -> crate::Result<&str> {
     static JS_URL_PATTERNS: SyncLazy<Regex> = SyncLazy::new(||
         Regex::new(r"(/s/player/[\w\d]+/[\w\d_/.]+/base\.js)").unwrap()
@@ -431,8 +440,8 @@ fn get_ytplayer_js(html: &str) -> crate::Result<&str> {
 
 /// Extracts a complete json object from a string. 
 #[inline]
-#[cfg(any(feature = "fetch", doc))]
 #[doc(cfg(feature = "fetch"))]
+#[cfg(any(feature = "fetch", doc))]
 fn json_object(mut html: &str) -> crate::Result<&str> {
     html = html.trim_start_matches(|c| c != '{');
     if html.is_empty() {
@@ -461,8 +470,8 @@ fn json_object(mut html: &str) -> crate::Result<&str> {
 
 /// Checks if a char represents the end of a json object.
 #[inline]
-#[cfg(any(feature = "fetch", doc))]
 #[doc(cfg(feature = "fetch"))]
+#[cfg(any(feature = "fetch", doc))]
 fn is_json_object_end(curr_char: u8, skip: &mut bool, stack: &mut Vec<u8>) -> bool {
     if *skip {
         *skip = false;
@@ -488,4 +497,22 @@ fn is_json_object_end(curr_char: u8, skip: &mut bool, stack: &mut Vec<u8>) -> bo
     }
 
     stack.is_empty()
+}
+
+pub fn recommended_cookies() -> reqwest::cookie::Jar {
+    let cookie = "CONSENT=YES+; Path=/; Domain=youtube.com; Secure; Expires=Sun, 10 Jan 2038 07:59:59 GMT;";
+    let url = "https://youtube.com".parse().unwrap();
+
+    let jar = reqwest::cookie::Jar::default();
+    jar.add_cookie_str(cookie, &url);
+    jar
+}
+
+pub fn recommended_headers() -> reqwest::header::HeaderMap {
+    let mut headers = reqwest::header::HeaderMap::new();
+
+    headers.insert(reqwest::header::ACCEPT_LANGUAGE, "en-US,en".parse().unwrap());
+    headers.insert(reqwest::header::USER_AGENT, "Mozilla/5.0".parse().unwrap());
+
+    headers
 }
