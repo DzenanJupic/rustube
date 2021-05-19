83e5e4ae4ad8f9e6242d0630d8b55429648c8e4b
Dzenan Jupic <56133904+DzenanJupic@users.noreply.github.com>
2021-05-19 12:52:44 +0200

    Changed VideoInfo serialization to normal

--- src/video_info/mod.rs
+++ src/video_info/mod.rs
@@ -10,7 +10,7 @@ pub mod player_response;
 #[serde_as]
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 pub struct VideoInfo {
-    #[serde_as(as = "JsonString")]
+    #[serde_as(deserialize_as = "JsonString")]
     pub player_response: PlayerResponse,
     #[serde(rename = "adaptive_fmts")]
     pub adaptive_fmts_raw: Option<String>,
