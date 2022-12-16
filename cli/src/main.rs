7f819bb894abda8d697a7d980794daabcce2b1cf
Dzenan Jupic <56133904+DzenanJupic@users.noreply.github.com>
2022-12-16 20:17:16 +0100

    A few small improvements to the progress bar
    - Make the progress bar a CLI option
    - Print progress bar to stderr
    - Remove the use of `blocking`
    - Add error handling
    - Get rid of redundant `counter` variable
    
    Signed-off-by: Dzenan Jupic <56133904+DzenanJupic@users.noreply.github.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,8 +5,8 @@ use clap::Parser;
 
 use args::DownloadArgs;
 use args::StreamFilter;
-use rustube::Callback;
 use rustube::{Error, Id, IdBuf, Stream, Video, VideoFetcher, VideoInfo};
+use rustube::Callback;
 
 use crate::args::{CheckArgs, Command, FetchArgs};
 use crate::video_serializer::VideoSerializer;
@@ -65,37 +65,25 @@ async fn download(args: DownloadArgs) -> Result<()> {
     let (video_info, stream) = get_stream(id.as_owned(), args.stream_filter).await?;
     let download_path = download_path(args.filename, stream.mime.subtype().as_str(), args.dir, id);
 
-    // init CLI progress bar
-    let mut pb = pbr::ProgressBar::new(stream.content_length().await?);
-    pb.set_units(pbr::Units::Bytes);
-    pb.format("╢▌▌░╟");
+    let mut pb = args.logging.init_progress_bar(stream.content_length().await?);
+    let callback = Callback::new()
+        .connect_on_progress_closure(|cargs| {
+            // update progress bar
+            pb.add(cargs.current_chunk.saturating_sub(cargs.current_chunk) as u64);
+        });
 
-    // handle download progress updates
-    let mut callback = Callback::new();
-    let mut counter = 0;
-    callback = callback.connect_on_progress_closure( move |cargs| {
-        // update progress bar
-        pb.add(cargs.current_chunk.saturating_sub(counter) as u64);
-        counter = cargs.current_chunk;
-    });
+    stream
+        .download_to_with_callback(&download_path, callback)
+        .await?;
+    pb.finish_println(&format!("Finished downloading video to {download_path:?}\n"));
 
-    let output_level = args.output.output_level;
-    let output = args.output;
-    let handle = std::thread::spawn(move || {
-        // TODO handle result
-        let _ = stream.blocking_download_to_with_callback(download_path, callback);
-
-        let video_serializer = VideoSerializer::new(
-            video_info,
-            std::iter::once(stream),
-            output_level,
-        );
-        let output = output.output_format.serialize_output(&video_serializer).unwrap();
-        println!("{}", output);
-    });
-
-    // wait for download to finish
-    let _ = handle.join();
+    let video_serializer = VideoSerializer::new(
+        video_info,
+        std::iter::once(stream),
+        args.output.output_level,
+    );
+    let output = args.output.output_format.serialize_output(&video_serializer).unwrap();
+    println!("{}", output);
 
     Ok(())
 }
@@ -126,7 +114,7 @@ async fn get_stream(id: IdBuf, stream_filter: StreamFilter) -> Result<(VideoInfo
 async fn get_streams(
     id: IdBuf,
     stream_filter: &'_ StreamFilter,
-) -> Result<(VideoInfo, impl Iterator<Item = Stream> + '_)> {
+) -> Result<(VideoInfo, impl Iterator<Item=Stream> + '_)> {
     let (video_info, streams) = get_video(id).await?.into_parts();
 
     let streams = streams
